5516c88f0750415cd000b18c01fcbd3ff3e5790c
Arthur Walsh <walsh.arthur@outlook.com>
2020-07-24 12:33:32 +0200

    Tabs on HomeScreen no ui

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 
 import * as LocalNotification from 'utils/notification/LocalNotification'
 import Title from 'elements/text/Title'
 import Screen from 'elements/layout/Screen'
 import HorizontalRule from 'elements/layout/HorizontalRule'
 import Button from 'elements/buttons/BaseButton'
+import PrayerScreen from 'screens/PrayersScreen'
 
 const Home = () => {
   const navigation = useNavigation()
+
   return (
     <Screen>
       <Title>Register for notifications</Title>
@@ -28,4 +31,15 @@ const Home = () => {
   )
 }
 
-export default Home
+const Tabs = createMaterialTopTabNavigator()
+
+const HomeScreen = () => {
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen name="Home" component={Home} />
+      <Tabs.Screen name="Prayers" component={PrayerScreen} />
+    </Tabs.Navigator>
+  )
+}
+
+export default HomeScreen
