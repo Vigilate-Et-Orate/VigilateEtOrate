49c0115c449555ab66d5b7886a7330532c16dfd5
Arthur Walsh <walsh.arthur@outlook.com>
2020-09-27 23:15:10 +0200

    page design + modal design

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import {
   View,
   Text,
@@ -7,172 +7,175 @@ import {
   StyleSheet,
   TouchableOpacity,
   ToastAndroid,
-  TextInput,
+  Image,
   ScrollView
 } from 'react-native'
-import * as Analytics from 'expo-firebase-analytics'
+import { BlurView } from 'expo-blur'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
 
 import * as Storage from 'utils/storage/StorageManager'
-import { MyPrayer } from 'config/types/Prayer'
-import { Header } from 'elements/text/Text'
 import theme from 'config/theme'
 
 const Settings = (): JSX.Element => {
+  const navigation = useNavigation()
   const [modalVisible, setModalVisible] = useState(false)
-  const [firstName, onFirstNameChange] = useState('')
-  const [prayerTitle, onPrayerTitleChange] = useState('')
-  const [prayerContent, onPrayerContentChange] = useState('')
-
-  useEffect(() => {
-    Storage.getDataAsync(Storage.Stored.MY_PRAYER).then((data) => {
-      if (!data) return
-      const myPrayer: MyPrayer = JSON.parse(data)
-      onPrayerTitleChange(myPrayer.title)
-      onPrayerContentChange(myPrayer.content)
-    })
-    Storage.getDataAsync(Storage.Stored.FIRSTNAME).then((data) => {
-      if (!data) return
-      onFirstNameChange(data)
-    })
-  }, [])
-
-  const saveAllData = () => {
-    const prayer: MyPrayer = {
-      title: prayerTitle,
-      content: prayerContent
-    }
-    Storage.setDataAsync(Storage.Stored.MY_PRAYER, JSON.stringify(prayer))
-    Storage.setDataAsync(Storage.Stored.FIRSTNAME, firstName)
-    Analytics.logEvent('profile', {
-      type: 'updatedPersonnal Information'
-    })
-    ToastAndroid.showWithGravity(
-      'Sauvergardé avec succès !',
-      ToastAndroid.SHORT,
-      ToastAndroid.BOTTOM
-    )
-  }
 
   return (
-    <ScrollView style={{ paddingHorizontal: 20, marginTop: 20 }}>
-      <Modal animationType="slide" transparent={true} visible={modalVisible}>
+    <View style={styles.background}>
+      <Modal animationType="fade" transparent={true} visible={modalVisible}>
         <View style={styles.centeredView}>
-          <View style={styles.modalView}>
+          <BlurView intensity={300} style={styles.modal}>
             <TouchableOpacity
-              style={{ alignSelf: 'flex-end' }}
+              style={{
+                alignSelf: 'flex-end',
+                backgroundColor: theme.colors.blue,
+                padding: 5,
+                borderRadius: 30
+              }}
               onPress={() => setModalVisible(!modalVisible)}
             >
-              <Text>X</Text>
+              <MaterialCommunityIcons
+                name="close"
+                size={20}
+                color={theme.colors.white}
+              />
             </TouchableOpacity>
-            <Text style={{ marginBottom: 10 }}>
+            <Text
+              style={{
+                marginBottom: 10,
+                color: theme.colors.blue,
+                fontSize: 18
+              }}
+            >
               Ceci est une opération destructive. Attention cela supprimera
               toutes vos données et souscriptions !
             </Text>
-            <Button
-              color="red"
-              title="Oui Supprimer toutes les souscriptions"
-              onPress={() => {
-                Storage.clear()
-                setModalVisible(!modalVisible)
-                ToastAndroid.showWithGravity(
-                  'Cache supprimé',
-                  ToastAndroid.SHORT,
-                  ToastAndroid.BOTTOM
-                )
-              }}
+            <Text style={{ color: theme.colors.red, fontSize: 16 }}>
+              Êtes vous sûr de vouloir tout effacer ?
+            </Text>
+            <View style={{ flexDirection: 'row-reverse', marginTop: 20 }}>
+              <TouchableOpacity
+                style={styles.modalButton}
+                onPress={() => {
+                  Storage.clear()
+                  setModalVisible(!modalVisible)
+                  ToastAndroid.showWithGravity(
+                    'Cache supprimé',
+                    ToastAndroid.SHORT,
+                    ToastAndroid.BOTTOM
+                  )
+                }}
+              >
+                <Text style={{ color: theme.colors.white, fontSize: 18 }}>
+                  Oui
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </BlurView>
+        </View>
+      </Modal>
+      <View style={styles.header}>
+        <View
+          style={{ flexDirection: 'row', height: '60%', position: 'relative' }}
+        >
+          <View style={{ position: 'absolute', top: 20, left: '20%' }}>
+            <Image
+              style={{ width: 80, height: 70 }}
+              source={require('../assets/newIconolive.png')}
             />
           </View>
         </View>
-      </Modal>
-      <View style={{ marginBottom: 15 }}>
-        <Header>Informations Personnelles</Header>
-        <Text style={styles.Label}>Prénom</Text>
-        <TextInput
-          style={styles.Input}
-          onChangeText={onFirstNameChange}
-          value={firstName}
-        />
-      </View>
-      <View style={{ marginBottom: 15 }}>
-        <Header>Ma Prière Personnelle</Header>
-        <Text style={styles.Label}>Titre</Text>
-        <TextInput
-          style={styles.Input}
-          onChangeText={onPrayerTitleChange}
-          value={prayerTitle}
-        />
-        <Text style={styles.Label}>La Prière</Text>
-        <TextInput
-          style={styles.Input}
-          onChangeText={onPrayerContentChange}
-          value={prayerContent}
-          multiline
-        />
-      </View>
-      <View style={{ flexDirection: 'row-reverse', marginBottom: 20 }}>
-        <View style={{ width: '40%' }}>
-          <Button
-            title="Enregistrer"
-            color={theme.colors.yellow}
-            onPress={saveAllData}
-          />
+        <View
+          style={{
+            flexDirection: 'row-reverse',
+            height: '40%',
+            paddingHorizontal: 40
+          }}
+        >
+          <Text style={{ color: '#f6f4f4', fontSize: 32 }}>Paramètres</Text>
         </View>
       </View>
-      <Button
-        onPress={() => {
-          setModalVisible(true)
-        }}
-        color={theme.colors.red}
-        title="Clear Cache and Notification Subscriptions"
-      />
-    </ScrollView>
+      <ScrollView style={styles.body}>
+        <TouchableOpacity
+          style={{
+            position: 'absolute',
+            top: '12%',
+            left: '2%',
+            elevation: 20,
+            zIndex: 90
+          }}
+          onPress={() => navigation.goBack()}
+        >
+          <MaterialCommunityIcons
+            name="arrow-left"
+            size={40}
+            color={theme.colors.white}
+          />
+        </TouchableOpacity>
+        <View style={styles.roundedView}>
+          <View style={{ paddingHorizontal: 20, marginTop: 20 }}>
+            <Button
+              onPress={() => {
+                setModalVisible(true)
+              }}
+              color={theme.colors.red}
+              title="Clear Cache and Notification Subscriptions"
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   centeredView: {
-    flex: 1,
+    paddingTop: '45%',
+    paddingHorizontal: 20,
+    flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22
+    alignItems: 'center'
   },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
+  modal: {
     borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5
-  },
-  Input: {
-    backgroundColor: '#ffffff',
-    borderRadius: 10,
     paddingHorizontal: 10,
-    paddingVertical: 5,
-    borderWidth: 0.5,
-    borderColor: theme.colors.blue
+    paddingVertical: 15
   },
-  TextInput: {
-    backgroundColor: '#ffffff',
-    borderRadius: 10,
+  modalButton: {
+    borderRadius: 30,
+    backgroundColor: theme.colors.red,
+    paddingHorizontal: 20,
+    paddingVertical: 10
+  },
+  background: {
+    height: '100%',
+    backgroundColor: theme.colors.blue
+  },
+  body: {
+    height: '125%'
+  },
+  header: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 0,
+    width: '100%',
+    height: '15%',
+    flex: 1,
     paddingHorizontal: 10,
-    paddingVertical: 5,
-    borderWidth: 0.5,
-    borderColor: theme.colors.blue,
-    height: 150
+    paddingTop: 30
   },
-  Label: {
-    marginLeft: 10,
-    color: theme.colors.blue,
-    fontSize: 14,
-    marginTop: 10
+  roundedView: {
+    marginTop: '40%',
+    backgroundColor: theme.colors.white,
+    borderRadius: 30,
+    paddingTop: 50,
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    paddingBottom: 35,
+    height: '100%'
   }
 })
 
