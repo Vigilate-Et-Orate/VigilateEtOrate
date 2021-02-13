f07f6b330bc229c317d41dea735da5f7ca829a77
Arthur Walsh <walsh.arthur@outlook.com>
2021-02-13 16:45:22 +0100

    fix encoding of rss saint field

--- utils/api/rss_nominis.ts
+++ utils/api/rss_nominis.ts
@@ -1,9 +1,24 @@
 import * as rssParser from 'react-native-rss-parser'
+import iconv from 'iconv-lite'
+import axios from 'axios'
+import { Buffer } from 'buffer'
+
 import { TNominisSaint } from 'config/types/Nominis'
+import * as Storage from 'utils/storage/StorageManager'
 import URL from 'config/url.config.json'
 
 export const getNominisSaint = async (): Promise<TNominisSaint | undefined> => {
-  const resData = await fetch(URL.NOMINIS_RSS).then((res) => res.text())
+  const resData = await axios({
+    method: 'GET',
+    url: URL.NOMINIS_RSS,
+    responseType: 'arraybuffer'
+  }).then((res) => {
+    const decoded = iconv.decode(
+      Buffer.from(res.data as ArrayBuffer),
+      'iso-8859-1'
+    )
+    return decoded
+  })
   const rss = await rssParser.parse(resData)
   if (!rss) return
   const rssSaint = rss.items[0]
@@ -12,5 +27,6 @@ export const getNominisSaint = async (): Promise<TNominisSaint | undefined> => {
     url: rssSaint.links[0].url,
     description: rssSaint.description
   }
+  await Storage.setDataAsync(Storage.Stored.DAY_INFO, saint)
   return saint
 }
